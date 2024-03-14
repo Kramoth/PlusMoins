c076a8a88519b7ea2a44ad86acfb5ecc7d9dc542
kramoth <kramoth@udm.ac.mu>
2024-03-14 10:52:35 +0400

    adding main function and fixing error in plus moins

--- src/plus_moins.c
+++ src/plus_moins.c
@@ -4,8 +4,17 @@ void plus_moins(int choix_joueur, int valeur_a_trouver){
 
 	if(choix_joueur<valeur_a_trouver)
 		printf("C'est plus !\n");
-	else if (choix_joueur_joueur>valeur_a_trouver)
+	else if (choix_joueur>valeur_a_trouver)
 		printf("C'est moins !\n");
 	else
 		printf("bingo\n");
+}
+
+int main()
+{
+	int guess=42;
+	int choix_joueur;
+	printf("Devinez un nombre entre 0 et 100:\n");
+	scanf("%d",&choix_joueur);
+	plus_moins(choix_joueur, guess);
 }
