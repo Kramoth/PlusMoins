68b56593c2945961ccfd5bb3f10139c8e529b7de
kramoth <kramoth@udm.ac.mu>
2024-03-14 10:46:47 +0400

    creation de la fonction plus moins

---
+++ src/plus_moins.c
@@ -0,0 +1,11 @@
+#include<stdio.h>
+
+void plus_moins(int choix_joueur, int valeur_a_trouver){
+
+	if(choix_joueur<valeur_a_trouver)
+		printf("C'est plus !\n");
+	else if (choix_joueur_joueur>valeur_a_trouver)
+		printf("C'est moins !\n");
+	else
+		printf("bingo\n");
+}
